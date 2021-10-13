e2e9e308eaf11bb77ce93ae4347b11c85c84d242
Tselmeg <g.tselmeg@yahoo.com>
2021-10-13 13:36:32 +0200

    unique email

--- model/user.js
+++ model/user.js
@@ -14,7 +14,8 @@ module.exports = (sequelize, Sequelize) => {
   },
   email: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   privacy: {
     type: Sequelize.BOOLEAN,
